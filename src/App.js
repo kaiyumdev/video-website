cd1a72c10692d9cf9e9be328bd81d4f0606e433f
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-11-16 16:01:43 +0600

    setup-reuting by react-router-dom successfully

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 // import Home from "./pages/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Video from "./pages/Video";
+import Home from "./pages/Home";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
-    return <Video />;
+  return (
+    <BrowserRouter>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/videos/:videoId" element={<Video></Video>}></Route>
+      </Routes>
+      <Footer></Footer>
+    </BrowserRouter>
+  );
 }
 
 export default App;
