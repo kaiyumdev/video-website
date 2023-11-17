df2d03f12207624ca38752ea809c12b50799a4fa
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-11-17 22:11:47 +0600

    updated 6.10 branches

--- src/features/relatedVideos/relatedVideosAPI.js
+++ src/features/relatedVideos/relatedVideosAPI.js
@@ -1,6 +1,6 @@
 import axiosInstance from "../../utils/axios";
 
-export const getRelatedVideos = async () => {
+export const getRelatedVideos = async ({ tags, id }) => {
   const response = await axiosInstance.get("/videos");
   return response.data;
 };
