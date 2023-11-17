6515db2e6d9fa12b3fc1025be2f03366de0b07a1
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-11-17 18:09:34 +0600

    start video features

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,8 +1,27 @@
+import { useDispatch, useSelector } from "react-redux";
 import VideoPlayer from "../components/description/Player";
 import VideoDescription from "../components/description/VideoDescription";
 import RelatedVideoList from "../components/list/RelatedVideoList";
+import { useEffect } from "react";
+import { fetchVideo } from "../features/video/videoSlice";
+import Loading from "../components/ui/Loading";
 
 export default function Video() {
+  const dispatch = useDispatch();
+  const { isLoading, isError, error } = useSelector((state) => state.video);
+  useEffect(() => {
+    dispatch(fetchVideo());
+  }, [dispatch]);
+
+  let content = null;
+  if (isLoading) content = <Loading></Loading>;
+
+  if (!isLoading && isError)
+    content = <div className="col-span-12">{error}</div>;
+
+  // if (!isLoading && !isError && video?.id)
+  // content = <div className="col-span-12">No videos found</div>;
+
   return (
     <>
       <section class="pt-6 pb-20">
